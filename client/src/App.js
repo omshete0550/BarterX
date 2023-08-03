8be6f939287e9e00084fa361ba890350670c1ab2
Mohib Abbas Sayed <m0hibsayed1393@gmail.com>
2023-08-03 22:44:56 +0530

    comment deleted

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Pages/Login/Login';
 import Landing from './Pages/Landing/Landing';
-import logo from './logo.svg';
 import Register from './Pages/Register/Register';
 function App() {
   return (
