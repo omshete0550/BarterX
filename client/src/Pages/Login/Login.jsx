b0bc979030157fece070357b7ebefa7f82b15741
Om <omshete0550@gmail.com>
2023-08-05 11:52:45 +0530

    login page updated with forgot password feature

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../Login/Login.css';
-import { TfiEmail, TfiLock } from 'react-icons/tfi';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import ForgotPasswordPopup from "./ForgotPasswordPopup";
+import "../Login/Login.css";
+import { TfiEmail, TfiLock } from "react-icons/tfi";
 
 const Login = () => {
+  const [showForgotPasswordPopup, setShowForgotPasswordPopup] = useState(false);
+
+  const handleForgotPasswordClick = () => {
+    setShowForgotPasswordPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowForgotPasswordPopup(false);
+  };
 
   return (
     <>
       <div className="LoginPageContainer">
         <div className="LoginPageInnerContainer">
-
           <div className="ImageContianer">
             <img src="https://i.imgur.com/MYZd7of.png" className="GroupImage" />
           </div>
@@ -17,52 +26,79 @@ const Login = () => {
           <div className="LoginFormContainer">
             <div className="LoginFormInnerContainer">
               <header className="header">Log in</header>
-              <header className="subHeader">Welcome to <b>BarterX!</b> Please Enter your Details</header>
+              <header className="subHeader">
+                Welcome to <b>BarterX!</b> Please Enter your Details
+              </header>
               <form>
                 <div className="inputContainer">
                   <label className="label" htmlFor="emailAddress">
-                    <i><TfiEmail /></i>
+                    <i>
+                      <TfiEmail />
+                    </i>
                     <span>Email Address*</span>
                   </label>
-                  <input type="email" className="input" id="emailAddress" name='email'  placeholder="Enter your Email Address"/>
+                  <input
+                    type="email"
+                    className="input"
+                    id="emailAddress"
+                    name="email"
+                    placeholder="Enter your Email Address"
+                    required
+                  />
                 </div>
                 <div className="inputContainer">
                   <label className="label" htmlFor="emailAddress">
-                    <i><TfiLock /></i>
+                    <i>
+                      <TfiLock />
+                    </i>
                     <span>Password*</span>
                   </label>
-                  <input type="password" className="input" id="emailAddress" name='password' placeholder="Enter your Password" />
+                  <input
+                    type="password"
+                    className="input"
+                    id="emailAddress"
+                    name="password"
+                    placeholder="Enter your Password"
+                    required
+                  />
                 </div>
                 <div className="OptionsContainer">
                   <div className="checkboxContainer">
-                    <input type="checkbox" id="RememberMe" className="checkbox" /> 
-                    <label for="RememberMe">Remember me</label>
+                    <input
+                      type="checkbox"
+                      id="RememberMe"
+                      className="checkbox"
+                    />
+                    <label htmlFor="RememberMe">Remember me</label>
                   </div>
-                    <a href="#" className="ForgotPasswordLink">Forgot Password?</a>
+                  <a
+                    href="#"
+                    className="ForgotPasswordLink"
+                    onClick={handleForgotPasswordClick}
+                  >
+                    Forgot Password?
+                  </a>
                 </div>
-                {/* <div className="OptionsContainer">
-                  <div className="checkboxContainer">
-                    <span> Don't have an account? <Link to="/register" style={{ color: '#5b80d1' }}>Sign Up</Link> </span>
-                  </div>
-                  <a href="#" className="ForgotPasswordLink">Forgot Password?</a>
-                </div> */}
                 <button className="LoginButton">Login</button>
-                <div className='HaveAnAcc'>
-                  <span> Don't have an account? {" "}
-                    <Link to="/register" style={{ color: '#5b80d1' }}>Sign Up</Link> 
+                <div className="HaveAnAcc">
+                  <span>
+                    {" "}
+                    Don't have an account?{" "}
+                    <Link to="/register" style={{ color: "#31708e" }}>
+                      Sign Up
+                    </Link>
                   </span>
                 </div>
               </form>
-
             </div>
-
           </div>
         </div>
       </div>
-
-
+      {showForgotPasswordPopup && (
+        <ForgotPasswordPopup onClose={handleClosePopup} />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../Login/Login.css';
-import { TfiEmail, TfiLocationPin, TfiLock, TfiUser } from 'react-icons/tfi'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../Login/Login.css";
+import { TfiEmail, TfiLocationPin, TfiLock, TfiUser } from "react-icons/tfi";
 
 const Register = () => {
+  const namePattern = /^[A-Za-z\s]+$/;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("success");
+  };
 
   return (
     <>
       <div className="LoginPageContainer">
         <div className="LoginPageInnerContainer">
-
           <div className="ImageContianer">
             <img src="https://i.imgur.com/MYZd7of.png" className="GroupImage" />
           </div>
@@ -17,59 +23,104 @@ const Register = () => {
           <div className="LoginFormContainer">
             <div className="LoginFormInnerContainer">
               <header className="header">Register Now</header>
-              <header className="subHeader">Welcome to <b>BarterX!</b> Please Enter your Details</header>
-              <form>
+              <header className="subHeader">
+                Welcome to <b>BarterX!</b> Please Enter your Details
+              </header>
+              <form onSubmit={handleSubmit}>
                 <div className="inputContainer">
-                    <label className="label" htmlFor="Name">
-                        <i><TfiUser /></i>
-                        <span>Name*</span>
-                    </label>
-                    <input type="text" className="input" id="emailAddress" placeholder="Enter your name" name='name' />
+                  <label className="label" htmlFor="Name">
+                    <i>
+                      <TfiUser />
+                    </i>
+                    <span>Name*</span>
+                  </label>
+                  <input
+                    type="text"
+                    className="input"
+                    id="name"
+                    placeholder="Enter your name"
+                    name="name"
+                    pattern={namePattern}
+                    required
+                  />
                 </div>
                 <div className="inputContainer">
                   <label className="label" htmlFor="emailAddress">
-                    <i><TfiEmail /></i>
+                    <i>
+                      <TfiEmail />
+                    </i>
                     <span>Email Address*</span>
                   </label>
-                  <input type="email" className="input" id="emailAddress" name='email'  placeholder="Enter your Email Address"/>
+                  <input
+                    type="email"
+                    className="input"
+                    id="emailAddress"
+                    name="email"
+                    placeholder="Enter your Email Address"
+                    required
+                  />
                 </div>
                 <div className="inputContainer">
                   <label className="label" htmlFor="emailAddress">
-                    <i><TfiLock /></i>
+                    <i>
+                      <TfiLock />
+                    </i>
                     <span>Password*</span>
                   </label>
-                  <input type="password" className="input" id="emailAddress" name='password' placeholder="Enter your Password" />
+                  <input
+                    type="password"
+                    className="input"
+                    id="password"
+                    name="password"
+                    placeholder="Enter your Password"
+                    required
+                  />
                 </div>
                 <div className="inputContainer">
-                    <label className="label" htmlFor="Name">
-                        <i><TfiLocationPin /></i>
-                        <span>Pincode*</span>
-                    </label>
-                    <input type="number" className="input" id="emailAddress" placeholder="Enter your pincode" name='name' />
+                  <label className="label" htmlFor="Name">
+                    <i>
+                      <TfiLocationPin />
+                    </i>
+                    <span>Pincode*</span>
+                  </label>
+                  <input
+                    type="number"
+                    className="input"
+                    id="pincode"
+                    placeholder="Enter your pincode"
+                    name="pincode"
+                    min={100000}
+                    max={999999}
+                    required
+                  />
                 </div>
                 <div className="OptionsContainer">
                   <div className="checkboxContainer">
-                    <input type="checkbox" id="RememberMe" className="checkbox" /> 
+                    <input
+                      type="checkbox"
+                      id="RememberMe"
+                      className="checkbox"
+                    />
                     <label for="RememberMe">Remember me</label>
                   </div>
                 </div>
                 <button className="LoginButton">Register</button>
-                <div className='HaveAnAcc'>
-                  <span> Already have an account? {" "}
-                    <Link to="/login" style={{ color: '#5b80d1' }}>Sign In</Link> 
+                <div className="HaveAnAcc">
+                  <span>
+                    {" "}
+                    Already have an account?{" "}
+                    <Link to="/login" style={{ color: "#5b80d1" }}>
+                      Sign In
+                    </Link>
                   </span>
                 </div>
               </form>
-
             </div>
-
           </div>
         </div>
       </div>
-
-
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
