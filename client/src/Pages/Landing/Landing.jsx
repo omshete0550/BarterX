58478c95321c1bf26d0660bdbc073b883d017c84
Om <omshete0550@gmail.com>
2023-09-22 11:10:54 +0530

    update

--- client/src/Pages/Landing/Landing.jsx
+++ client/src/Pages/Landing/Landing.jsx
@@ -126,16 +126,30 @@ const Landing = () => {
           transition={{ duration: 0.5 }}
         >
           <Link to="/home ">
-            <button className="HomeBtn">
-              H O M E
-              <div id="clip">
-                <div id="leftTop" className="corner"></div>
-                <div id="rightBottom" className="corner"></div>
-                <div id="rightTop" className="corner"></div>
-                <div id="leftBottom" className="corner"></div>
-              </div>
-              <span id="rightArrow" className="arrow"></span>
-              <span id="leftArrow" className="arrow"></span>
+            <button class="HomeButton">
+              <svg
+                stroke="#ffffff"
+                viewBox="0 0 80 80"
+                xmlns="http://www.w3.org/2000/svg"
+                id="Capa_1"
+                version="1.1"
+                fill="#ffffff"
+              >
+                <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                <g
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  id="SVGRepo_tracerCarrier"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <g>
+                    {" "}
+                    <path d="M64,48L64,48h-8V32h8c8.836,0,16-7.164,16-16S72.836,0,64,0c-8.837,0-16,7.164-16,16v8H32v-8c0-8.836-7.164-16-16-16 S0,7.164,0,16s7.164,16,16,16h8v16h-8l0,0l0,0C7.164,48,0,55.164,0,64s7.164,16,16,16c8.837,0,16-7.164,16-16l0,0v-8h16v7.98 c0,0.008-0.001,0.014-0.001,0.02c0,8.836,7.164,16,16,16s16-7.164,16-16S72.836,48.002,64,48z M64,8c4.418,0,8,3.582,8,8 s-3.582,8-8,8h-8v-8C56,11.582,59.582,8,64,8z M8,16c0-4.418,3.582-8,8-8s8,3.582,8,8v8h-8C11.582,24,8,20.417,8,16z M16,72 c-4.418,0-8-3.582-8-8s3.582-8,8-8l0,0h8v8C24,68.418,20.418,72,16,72z M32,48V32h16v16H32z M64,72c-4.418,0-8-3.582-8-8l0,0v-8 h7.999c4.418,0,8,3.582,8,8S68.418,72,64,72z"></path>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+              Let's get started
             </button>
           </Link>
         </motion.div>
@@ -242,7 +256,6 @@ const Landing = () => {
           </motion.h2>
 
           <div className="Categrow">
-            
             <Link to="/categ">
               <motion.div
                 variants={textVariants("left", 0.2)}
@@ -450,7 +463,6 @@ const Landing = () => {
                 </div>
               </motion.div>
             </Link>
-
           </div>
         </section>
       </div>
