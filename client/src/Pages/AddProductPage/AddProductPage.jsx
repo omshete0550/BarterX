56f574c790fa03d5ab8134f5b1b2b15d952d6be4
Om <omshete0550@gmail.com>
2023-10-30 13:44:43 +0530

    cghn

--- client/src/Pages/AddProductPage/AddProductPage.jsx
+++ client/src/Pages/AddProductPage/AddProductPage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 
 import "./AddProductPage.css";
