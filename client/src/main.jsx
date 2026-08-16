10c008e798698e49152823d3d178af91828bd6f6
omshete0550 <omshete0550@gmail.com>
2026-08-16 23:49:14 +0530

    fix: add Vercel SPA routing and scroll-to-top navigation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./component/ScrollToTop";
 
 import App from "./App";
 import "./styles/global/globals.css";
@@ -8,6 +9,7 @@ import "./styles/global/globals.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
